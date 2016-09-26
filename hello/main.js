07a59b62c46df7235d0a10cad141cf85a60378cf
zhangshiyou <787900398@qq.com>
2016-09-26 17:41:00 +0800

    Signed-off-by: zhangshiyou <787900398@qq.com>

---
+++ hello/main.js
@@ -0,0 +1,8 @@
+var f="lorem";
+var dd=require('./l.js');
+//var $=require('./jquery-1.11.3.js')
+//dd();
+console.log(f);
+//$(".div").text("lorem");
+$(".p").html("这个webpack真好玩");
+alert("webpack 打包工具")
